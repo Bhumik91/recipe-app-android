b8b413007fb7be69520b9d02a43079b808257506
Bhumik Poshiya <bhumik.poshiya@simformsolutions.com>
2026-07-11 09:06:40 +0530

    TE-T733: Implemented app icon, custom colors, splash screen and fonts
    TE-T733: Added custom theme colors

---
+++ app/src/main/java/com/example/recipeapp/main/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.recipeapp.main
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.recipeapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+    }
+}
