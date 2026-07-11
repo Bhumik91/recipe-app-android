3882a708deaa2a30b4ddabc0dbc0ac9130c9495f
Bhumik Poshiya <bhumik.poshiya@simformsolutions.com>
2026-07-11 13:17:52 +0530

    TE-T733: Implemented navigation between sign-in and sign-up from clickable textview using intent

---
+++ app/src/main/java/com/example/recipeapp/features/auth/SignUpActivity.kt
@@ -0,0 +1,31 @@
+package com.example.recipeapp.features.auth
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.recipeapp.databinding.ActivitySignUpBinding
+
+class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        configureOnClicks()
+    }
+    private fun configureOnClicks() {
+        binding.tvSignIn.setOnClickListener {
+            finish()
+        }
+    }
+}
