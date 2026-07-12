292c3857147ed5dd325aadea88ae200603030fca
Bhumik Poshiya <bhumik.poshiya@simformsolutions.com>
2026-07-12 19:53:32 +0530

    TE-T733: Implemented OnBoardingActivity where flows goes after successful authentication

--- app/src/main/java/com/example/recipeapp/features/onboarding/OnBoardingActivity.kt
+++ app/src/main/java/com/example/recipeapp/features/onboarding/OnBoardingActivity.kt
@@ -4,9 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-
 import com.example.recipeapp.databinding.ActivityOnBoardingBinding
-import com.example.recipeapp.features.auth.SignInActivity
+import com.example.recipeapp.features.auth.view.LoginActivity
 
 class OnBoardingActivity : AppCompatActivity() {
 
@@ -22,7 +21,7 @@ class OnBoardingActivity : AppCompatActivity() {
 
     private fun configureOnClicks() {
         binding.btnStart.setOnClickListener {
-            startActivity(Intent(this, SignInActivity::class.java))
+            startActivity(Intent(this, LoginActivity::class.java))
             finish()
         }
     }
