3adc1e6556e9cbf1eb4dec0f20f3b3a989ed6834
Bhumik Poshiya <bhumik.poshiya@simformsolutions.com>
2026-07-12 19:39:21 +0530

    TE-T733: Implemented LoginRequest and LoginResponse models

---
+++ app/src/main/java/com/example/recipeapp/features/auth/model/AuthModels.kt
@@ -0,0 +1,19 @@
+package com.example.recipeapp.features.auth.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class LoginRequest(
+    @SerialName("username")
+    val userName: String,
+    val password: String,
+)
+@Serializable
+data class LoginResponse(
+    val id: Int,
+    @SerialName("firstName")
+    val name: String,
+    val accessToken: String,
+    val refreshToken: String
+)
